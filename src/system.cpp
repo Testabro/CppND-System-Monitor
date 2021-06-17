dd0fb852fa3c94b63ec634daa9322815278d8ad5
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2021-06-17 08:22:22 -0400

    feat: Get number of running processes from linux filesystem proc

--- src/system.cpp
+++ src/system.cpp
@@ -28,10 +28,9 @@ float System::MemoryUtilization() { return 0.0; }
 // TODO: Return the operating system name
 std::string System::OperatingSystem() { return string(); }
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); } //use /proc/stat
 
 long int System::UpTime() { return LinuxParser::UpTime(); }
